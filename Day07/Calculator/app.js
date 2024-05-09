140d28dd38bb4cbf27b7aafb99e6ea51fa4a76ad
Ayush Rastogi <ayushrastogi689@gmail.com>
2024-05-10 01:21:42 +0530

    Functionality - Added subtraction functionality.

--- Day07/Calculator/app.js
+++ Day07/Calculator/app.js
@@ -1,12 +1,20 @@
-//
-
-let resultField = document.querySelector(".resultField");
+let resultField = document.querySelector("#finalResult input");
 
 function add() {
   let firstNumber = parseInt(document.querySelector("#firstNumber").value);
   let secondNumber = parseInt(document.querySelector("#secondNumber").value);
 
   let resultValue = firstNumber + secondNumber;
+
+  resultField.setAttribute("type", "number");
+  resultField.value = resultValue;
+}
+
+function subtract() {
+  let firstNumber = parseInt(document.querySelector("#firstNumber").value);
+  let secondNumber = parseInt(document.querySelector("#secondNumber").value);
+
+  let resultValue = firstNumber - secondNumber;
 
   resultField.setAttribute("type", "number");
   resultField.value = resultValue;
